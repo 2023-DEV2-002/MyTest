86d7031a2416869a4b76b2d3c9fd775b3de56921
2023-DEV2-002 <none@none.none>
2023-01-30 18:31:45 +0100

    Showing first seconds lamp on ui

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
 import java.util.*
@@ -21,23 +26,34 @@ import java.util.*
 class MainActivity : ComponentActivity() {
 
 
-    private val currentTime: MutableState<String> = mutableStateOf(getCurrentFormattedTime())
+    private val currentTimeState: MutableState<String> = mutableStateOf(getCurrentFormattedTime())
+    private val secondsLampColorState: MutableState<Color> = mutableStateOf(Color.White)
+
+
+    private val berlinUhr = BerlinUhr()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val handler = Handler(mainLooper)
 
         handler.postDelayed(object : Runnable {
             override fun run() {
-                currentTime.value = getCurrentFormattedTime()
-
+                currentTimeState.value = getCurrentFormattedTime()
+                secondsLampColorState.value = berlinUhr.getSecondLampColor(berlinUhr.getSecondsComponent(currentTimeState.value))
                 handler.postDelayed(this, 1000)
             }
         }, 0)
 
         setContent {
-            RegularDigitalClock()
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                SecondsLamp()
+                RegularDigitalClock()
+            }
+
         }
     }
 
@@ -49,20 +65,35 @@ class MainActivity : ComponentActivity() {
     }
 
 
-
     /* Composable functions */
 
     @Composable
     fun RegularDigitalClock() {
-
-
-
         Box(Modifier.fillMaxWidth()) {
             Text(
-                text = currentTime.value,
+                text = currentTimeState.value,
                 textAlign = TextAlign.Center,
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+
+    @Composable
+    fun SecondsLamp() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(width = 3.dp, shape = CircleShape, color = Color.Black)
+                    .background(secondsLampColorState.value)
             )
         }
     }
