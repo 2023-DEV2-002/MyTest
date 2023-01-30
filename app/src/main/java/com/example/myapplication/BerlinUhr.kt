c7ec69f740e4c60b1741f056eddfed30c1083410
2023-DEV2-002 <none@none.none>
2023-01-30 18:09:44 +0100

    converting time string into a list of 3 elements - hour, minute, seconds

--- app/src/main/java/com/example/myapplication/BerlinUhr.kt
+++ app/src/main/java/com/example/myapplication/BerlinUhr.kt
@@ -5,15 +5,13 @@ import androidx.compose.ui.graphics.Color
 class BerlinUhr {
 
 
-
-
-
+    fun splitTimeToIntList(time: String) = time.split(":").map { it.toInt() }
 
 
     fun getSecondLampColor(sec: Int): Color {
         return if (sec % 2 == 0)
             Color.Black
-         else Color.White
+        else Color.White
     }
 
 
--- app/src/test/java/com/example/myapplication/BerlinUhrTest.kt
+++ app/src/test/java/com/example/myapplication/BerlinUhrTest.kt
@@ -9,6 +9,14 @@ class BerlinUhrTest {
 
 
     @Test
+    fun `convert string time into a list 3 elements`() {
+        val berlinUhr = BerlinUhr()
+        val time = berlinUhr.splitTimeToIntList("18:05:26")
+        Assert.assertEquals(3, time.size)
+    }
+
+
+    @Test
     fun `get black color when odd seconds are shown`() {
         val berlinUhr = BerlinUhr()
         val color = berlinUhr.getSecondLampColor(44)
