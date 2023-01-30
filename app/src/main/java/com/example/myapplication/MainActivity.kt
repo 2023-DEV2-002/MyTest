d593eb4ec64bb64cda55bbd50ae2f6463740d059
2023-DEV2-002 <oriol@Oriols-MacBook-Pro.local>
2023-01-30 17:12:04 +0100

    Just printing a regular digital clock

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,68 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
 
+
+    private val currentTime: MutableState<String> = mutableStateOf(
+        SimpleDateFormat(
+            "HH:mm:ss",
+            Locale.GERMANY
+        ).format(Date(System.currentTimeMillis())).toString()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+
+        val handler = Handler(mainLooper)
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                currentTime.value = SimpleDateFormat(
+                    "HH:mm:ss",
+                    Locale.GERMANY
+                ).format(Date(System.currentTimeMillis())).toString()
+
+                handler.postDelayed(this, 1000)
+            }
+        }, 0)
+
+
+        setContent {
+            RegularDigitalClock()
+        }
     }
+
+
+    @Composable
+    fun RegularDigitalClock() {
+        Box(Modifier.fillMaxWidth()) {
+            Text(
+                text = currentTime.value,
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
 }
 
 
